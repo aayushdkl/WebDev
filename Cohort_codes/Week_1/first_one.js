8d8ea1f28dc21c7b0d7b1bdbf8ed128f8fae1c42
aayushdkl <079bei001.aayush@sagarmatha.edu.np>
2024-09-18 19:13:18 +0545

    Json parse and stringify

---
+++ Cohort_codes/Week_1/first_one.js
@@ -0,0 +1,30 @@
+//console.log("Hello World!");
+
+// const a = 1000;
+
+// if (a % 2 == 0) {
+//   console.log("a is even");
+// } else {
+//   console.log("a is odd");
+// }
+
+// for (let i = 0; i <= a; i++) {
+//   console.log(i);
+// }
+
+//Json parse and stringify
+
+// const obj = '{"name":"aayush", "age": 2}';
+// // console.log(obj);
+// // console.log(typeof obj);
+
+// const user = JSON.parse(obj);
+// console.log(user);
+
+// const user = {
+//   name: "aayush",
+//   age: 2,
+// };
+
+// const obj = JSON.stringify(user);
+// console.log(obj);
