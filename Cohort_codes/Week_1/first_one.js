89e1b9bf81aacc62af19d8a2d76486cad5ec851a
aayushdkl <079bei001.aayush@sagarmatha.edu.np>
2024-09-20 23:55:00 +0545

    update on some files

--- Cohort_codes/Week_1/first_one.js
+++ Cohort_codes/Week_1/first_one.js
@@ -28,3 +28,18 @@
 
 // const obj = JSON.stringify(user);
 // console.log(obj);
+
+let p = new Promise((resolve, reject) => {
+  let a = 1 + 1;
+  if (a == 2) {
+    resolve("Success");
+  } else {
+    reject("Failed");
+  }
+});
+
+p.then((message) => {
+  console.log("This is in the then " + message);
+}).catch((message) => {
+  console.log("This is in the catch " + message);
+});
