25dff1de91044c74e3d7fc4262166930080bb09e
aayushdkl <079bei001.aayush@sagarmatha.edu.np>
2024-09-23 19:15:24 +0545

    2-clock problem solved

--- Cohort_codes/assignments-master/week-2/01-async-js/medium/1-file-cleaner.js
+++ Cohort_codes/assignments-master/week-2/01-async-js/medium/1-file-cleaner.js
@@ -1,21 +1,25 @@
 const fs = require("fs");
 
-fs.readFile("a.txt", "utf-8", (err, data) => {
-  if (err) {
-    console.log("Error reading the file");
-    return;
-  }
-
-  console.log("data read from the file is ");
-  console.log(data);
-
-  let new_data = data.replace(/\s+/g, " ");
-  fs.appendFile("a.txt", `\n${new_data}`, (err) => {
+fs.readFile(
+  "Cohort_codesassignments-masterweek-2\01-async-jsmediuma.txt",
+  "utf-8",
+  (err, data) => {
     if (err) {
-      console.log("Error writing the file");
+      console.log("Error reading the file");
       return;
     }
-    console.log("The updated file contains");
-    console.log(new_data);
-  });
-});
+
+    console.log("data read from the file is ");
+    console.log(data);
+
+    let new_data = data.replace(/\s+/g, " ");
+    fs.appendFile("a.txt", `\n${new_data}`, (err) => {
+      if (err) {
+        console.log("Error writing the file");
+        return;
+      }
+      console.log("The updated file contains");
+      console.log(new_data);
+    });
+  }
+);
---
+++ Cohort_codes/assignments-master/week-2/01-async-js/medium/2-clock.js
@@ -0,0 +1,15 @@
+setInterval(current_time, 1000);
+
+function current_time() {
+  let the_time = new Date();
+  let hours = addZero(the_time.getHours());
+  let minutes = addZero(the_time.getMinutes());
+  let seconds = addZero(the_time.getSeconds());
+
+  function addZero(n) {
+    return n < 10 ? `0${n}` : n;
+  }
+
+  console.log(`${hours}:${minutes}:${seconds}`);
+  console.log(the_time.toLocaleTimeString("en-US"));
+}
