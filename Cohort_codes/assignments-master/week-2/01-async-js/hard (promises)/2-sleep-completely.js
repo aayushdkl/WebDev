89b0013743637ac4b69cf775b541e180a43c6dcf
aayushdkl <079bei001.aayush@sagarmatha.edu.np>
2024-09-25 23:08:13 +0545

    all_hard_problem_solved

--- Cohort_codes/assignments-master/week-2/01-async-js/hard (promises)/2-sleep-completely.js
+++ Cohort_codes/assignments-master/week-2/01-async-js/hard (promises)/2-sleep-completely.js
@@ -5,6 +5,18 @@
  */
 
 function sleep(milliseconds) {
+  return new Promise((resolve) => {
+    // Step 1: Get the current time
+    let startTime = Date.now();
+
+    // Step 2: Busy wait using a while loop
+    while (Date.now() - startTime < milliseconds) {
+      // This loop keeps the thread busy and does nothing until the time has passed
+    }
+
+    // Step 3: After the loop, resolve the promise
+    resolve();
+  });
 }
 
 module.exports = sleep;
--- Cohort_codes/assignments-master/week-2/01-async-js/medium/2-clock.js
+++ Cohort_codes/assignments-master/week-2/01-async-js/medium/2-clock.js
@@ -6,10 +6,9 @@ function current_time() {
   let minutes = addZero(the_time.getMinutes());
   let seconds = addZero(the_time.getSeconds());
 
-  function addZero(n) {
-    return n < 10 ? `0${n}` : n;
-  }
-
   console.log(`${hours}:${minutes}:${seconds}`);
   console.log(the_time.toLocaleTimeString("en-US"));
+}
+function addZero(n) {
+  return n < 10 ? `0${n}` : n;
 }
