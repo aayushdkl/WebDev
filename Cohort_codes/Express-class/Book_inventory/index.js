68fed07ff1922545269a00fdf12f7706db67cbe0
aayushdkl <079bei001.aayush@sagarmatha.edu.np>
2024-10-03 23:36:30 +0545

    Book_inventory_code

--- Cohort_codes/Express-class/Book_inventory/index.js
+++ Cohort_codes/Express-class/Book_inventory/index.js
@@ -17,26 +17,32 @@
 
 const express = require("express");
 const app = express();
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
 const books = [
   //the big bracket is an array
   {
     //the curly bracket is an object
+    id: 1,
     title: "The Lord of the Rings",
     author: "J.R.R. Tolkien",
     available: true,
   },
   {
+    id: 2,
     title: "Muna madan",
     author: "Laxmi Prasad Devkota",
     available: false,
   },
   {
+    id: 3,
     title: "The Alchemist",
     author: "Paul",
     available: true,
   },
   {
+    id: 4,
     title: "Pride and Prejudice",
     author: "Jane Austen",
     available: false,
@@ -57,6 +63,35 @@ app.get("/", (req, res) => {
     available_books,
     unavailable_books,
   });
+});
+
+app.post("/", (req, res) => {
+  const new_book = req.body;
+  books.push(new_book);
+  res.json({ message: "New book added successfully", book: new_book });
+});
+
+app.put("/", (req, res) => {
+  const bookId = parseInt(req.params.id);
+  const updatedData = req.body;
+  for (let i = 0; i < books.length; i++) {
+    if (books[i].id === bookId) {
+      books[i].available = updatedData.available;
+      return res.json({ message: "Book availability updated", book: books[i] });
+    }
+  }
+  res.status(404).json({ message: "Book not found" });
+});
+
+app.delete("/:id", (req, res) => {
+  const bookId = parseInt(req.params.id);
+  for (let i = 0; i < books.length; i++) {
+    if (books[i].id === bookId) {
+      books.splice(i, 1);
+      return res.json({ message: "Book deleted successfully" });
+    }
+  }
+  res.status(404).json({ message: "Book not found" });
 });
 
 app.listen(3000);
